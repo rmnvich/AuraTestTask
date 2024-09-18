ac8efcc755fbc92c8a35a9697abd5f062af2a48d
vromanovich <romanovich.personal@gmail.com>
2024-09-18 21:49:17 +0200

    Updated notification icon

--- app/src/main/java/com/aura/test/vromanovich/presentation/notification/BootNotificationManagerImpl.kt
+++ app/src/main/java/com/aura/test/vromanovich/presentation/notification/BootNotificationManagerImpl.kt
@@ -15,7 +15,7 @@ class BootNotificationManagerImpl(
 
     override fun showNotification(body: String) {
         val notification = NotificationCompat.Builder(context, BOOTS_INFORMATION_CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setSmallIcon(R.mipmap.ic_launcher)
             .setContentTitle(context.getString(R.string.notification_boots_title))
             .setContentText(body)
             .build()
