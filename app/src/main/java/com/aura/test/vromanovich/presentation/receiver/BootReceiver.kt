d4e503cad74f5c077e35d6b8fd0c8175496af0a4
vromanovich <romanovich.personal@gmail.com>
2024-09-18 20:43:26 +0200

    Added logic of showing notification in BootReceiver

--- app/src/main/java/com/aura/test/vromanovich/presentation/receiver/BootReceiver.kt
+++ app/src/main/java/com/aura/test/vromanovich/presentation/receiver/BootReceiver.kt
@@ -3,13 +3,49 @@ package com.aura.test.vromanovich.presentation.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.aura.test.vromanovich.domain.usecase.IsNotificationShownUseCase
+import com.aura.test.vromanovich.presentation.notification.BootNotificationManager
+import com.aura.test.vromanovich.presentation.worker.NotificationWorker
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class BootReceiver : BroadcastReceiver() {
+
+    @Inject
+    lateinit var isNotificationShownUseCase: IsNotificationShownUseCase
 
     override fun onReceive(context: Context, intent: Intent) {
         if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
-            Log.d("BootReceiver", "BOOT_COMPLETED received");
+            // This is legal to use GlobalScope here, because this receiver lives while the process lives,
+            // and it doesn't bounded with any lifecycle component, so leak is impossible here.
+            @OptIn(DelicateCoroutinesApi::class)
+            GlobalScope.launch {
+                if (isNotificationShownUseCase()) {
+                    showNotification(context)
+                } else {
+                    scheduleNotification(context)
+                }
+            }
         }
+    }
+
+    private fun showNotification(context: Context) {
+        //TODO: Update logic of special body
+        BootNotificationManager.from(context).showNotification("Special body from receiver")
+    }
+
+    private fun scheduleNotification(context: Context) {
+        val notificationWork = OneTimeWorkRequest.Builder(NotificationWorker::class.java)
+            .setInitialDelay(15, TimeUnit.MINUTES)
+            .build()
+
+        WorkManager.getInstance(context).enqueue(notificationWork)
     }
 }
