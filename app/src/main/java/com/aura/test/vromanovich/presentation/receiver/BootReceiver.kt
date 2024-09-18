d2334823aa7f81e91d78bf91e7035d4b12f7a4eb
vromanovich <romanovich.personal@gmail.com>
2024-09-18 20:57:12 +0200

    Updated logic of BootReceiver

--- app/src/main/java/com/aura/test/vromanovich/presentation/receiver/BootReceiver.kt
+++ app/src/main/java/com/aura/test/vromanovich/presentation/receiver/BootReceiver.kt
@@ -4,14 +4,20 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkInfo
 import androidx.work.WorkManager
-import com.aura.test.vromanovich.domain.usecase.IsNotificationShownUseCase
+import com.aura.test.vromanovich.domain.usecase.notification.shown.get.IsNotificationShownUseCase
+import com.aura.test.vromanovich.domain.usecase.notification.shown.set.SetNotificationShownUseCase
 import com.aura.test.vromanovich.presentation.notification.BootNotificationManager
 import com.aura.test.vromanovich.presentation.worker.NotificationWorker
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.UUID
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
@@ -21,31 +27,52 @@ class BootReceiver : BroadcastReceiver() {
     @Inject
     lateinit var isNotificationShownUseCase: IsNotificationShownUseCase
 
+    @Inject
+    lateinit var setNotificationShownUseCase: SetNotificationShownUseCase
+
     override fun onReceive(context: Context, intent: Intent) {
         if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
             // This is legal to use GlobalScope here, because this receiver lives while the process lives,
             // and it doesn't bounded with any lifecycle component, so leak is impossible here.
             @OptIn(DelicateCoroutinesApi::class)
             GlobalScope.launch {
-                if (isNotificationShownUseCase()) {
-                    showNotification(context)
-                } else {
-                    scheduleNotification(context)
+                // TODO: Better provide interface with dispatchers using DI.
+                val isNotificationShown = withContext(Dispatchers.IO) {
+                    isNotificationShownUseCase()
+                }
+                withContext(Dispatchers.Main) {
+                    if (isNotificationShown) {
+                        showNotification(context)
+                    } else {
+                        scheduleNotification(context)
+                    }
                 }
             }
         }
     }
 
-    private fun showNotification(context: Context) {
+    private suspend fun showNotification(context: Context) {
         //TODO: Update logic of special body
-        BootNotificationManager.from(context).showNotification("Special body from receiver")
+        BootNotificationManager.from(context)
+            .showNotification("Special body from receiver")
+
+        withContext(Dispatchers.IO) {
+            setNotificationShownUseCase(true)
+        }
     }
 
-    private fun scheduleNotification(context: Context) {
+    private suspend fun scheduleNotification(context: Context) {
+        val notificationWorkId = UUID.randomUUID()
         val notificationWork = OneTimeWorkRequest.Builder(NotificationWorker::class.java)
             .setInitialDelay(15, TimeUnit.MINUTES)
+            .setId(notificationWorkId)
             .build()
 
-        WorkManager.getInstance(context).enqueue(notificationWork)
+        WorkManager.getInstance(context).run {
+            enqueue(notificationWork)
+            getWorkInfoByIdFlow(notificationWorkId)
+                .filter { it.state == WorkInfo.State.SUCCEEDED }
+                .collect { showNotification(context) }
+        }
     }
 }
